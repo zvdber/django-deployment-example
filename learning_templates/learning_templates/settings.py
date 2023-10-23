db79e1cc756ae3fde89564a4f5a821baff0b71f6
Zviadi.Berekashvili@coxautoinc.com <Zviadi.Berekashvili@coxautoinc.com>
2023-10-23 21:28:01 +0400

    allowed host is added

--- learning_templates/learning_templates/settings.py
+++ learning_templates/learning_templates/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#*-*40((1#l!bx__+s5u9kf6ta9t($u6&qa+ssq@hn4w70c!d^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['zvber.pythonanywhere.com']
 
 
 # Application definition
